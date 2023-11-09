55add0ee7f954a2293b434b93db12cc0af6772e9
SujithKumarSingaravel <147702718+SujithKumarSingaravel@users.noreply.github.com>
2023-11-09 12:16:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -23,8 +23,8 @@ resource "aws_security_group" "web_access123" {
                 cidr_blocks = ["0.0.0.0/0"]
         }
 }
-resource "aws_key_pair" "key123" {
-  key_name   = "key123"
+resource "aws_key_pair" "key1234" {
+  key_name   = "key1234"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC519pLvulS7oL8hVpHMnAgfTzpm3otd3oKvxEOPUhbOT7H3TH1azSx7qDA1Azmpe9kEkl63XrHe0AherilsFZXvWlap7jna5CtkO0RHUT2HIVCkHtTdmkyWyGq7ERS1VoLmSX/uq1A/VKxUhB60D5YgjnLbakopJ4r0HGWxPTRq8lPR+WI+i609dESRw0+q/QJfV4mR/et1D3JgoLDyiWZofOTaC9Dhkfoy9QOfCPNUh/DoxFqE9v5wdTMmBioW6M2KzXLj7gfhhvdrss6dXgBNU6FGHIAJHBTD5w3e0/sosOgd7B/0KCQGMRH2S1TzbqzOsgFxaWVtPVKx3yYYb9WPkiRD1LWJN6CE2DwcTtY/0FTSZLIPJuJLAjuBp9b47RzT0tfrvca/hcugfs1i4oTGr0bGcpEMRJi4dyfCnfhShqXF7PFO5JodxnCswmBsP2Wdrhs4xr/Wqpifl3Ig5/mohKGAP5Z3ctf5HAQX+clGq8vdkH8h2Uq87GzsLwBIms= root@ip-172-31-36-117.ap-south-1.compute.internal"
 }
  
@@ -32,7 +32,7 @@ resource "aws_instance" "newweb" {
   ami           = "ami-05c13eab67c5d8861"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.web_access123.name}"]
-  key_name = aws_key_pair.key123.key_name
+  key_name = aws_key_pair.key1234.key_name
   tags = {
     Name  = "Sujith-World"
     Stage = "testing"
