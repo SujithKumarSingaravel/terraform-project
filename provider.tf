e76f57bacb53d9b63a203ba9227bfe965f00e7d1
SujithKumarSingaravel <147702718+SujithKumarSingaravel@users.noreply.github.com>
2023-11-09 09:59:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
+  access_key = "AKIAQRU5QFQKCBMWQUWX"
+  secret_key = "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"
 }
  
 resource "aws_security_group" "web_access" {
